a8388003caac86ae1fde5bef5be397a1300b3fb3
Gáspár Nagy <gaspar.nagy@gmail.com>
2024-07-28 09:33:24 +0200

    add update-versions.ps1 script

---
+++ update-versions.ps1
@@ -0,0 +1,34 @@
+param(
+  [string] $version
+)
+
+function Set-Versions($content, $versionToSet) {
+
+
+  $newContent = [regex]::replace($content,'<PackageReference Include="(?<packageName>Reqnroll[^"]*)" Version="(?<version>[^"]*)" />', { 
+    param($m)
+    $m.Value.Replace($m.Groups["version"].Value, $versionToSet)
+  })
+  return $newContent
+}
+
+if (-not $version) {
+  Write-Error "Error. The version was not specified."
+  Write-Error "Usage:"
+  Write-Error "  update-versions.ps1 [version]"
+  Exit
+}
+
+$projectFiles = Get-ChildItem -Path $PSScriptRoot -File -Recurse -Filter '*.csproj'
+$changedCount = 0
+foreach ($path in $projectFiles){
+  $fileContent = Get-Content -LiteralPath $path -Raw
+  $newFileContent = Set-Versions $fileContent $version
+  if ($newFileContent -ne $fileContent){
+    Write-Host "Updating $($path.Name)"
+    Set-Content -Path $path -Value $newFileContent -NoNewline -Encoding utf8
+    $changedCount++
+  }
+}
+
+Write-Host "Updated $changedCount files."
