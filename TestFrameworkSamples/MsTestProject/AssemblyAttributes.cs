213a0b11cffd85efb012be8536b13c86f9c3b94e
Gáspár Nagy <gaspar.nagy@gmail.com>
2024-10-14 12:10:47 +0200

    Add assembly attribute samples to TestFrameworkSamples

---
+++ TestFrameworkSamples/MsTestProject/AssemblyAttributes.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// class-level parallel
+[assembly: Parallelize(Scope = ExecutionScope.ClassLevel)]
+
+// method-level parallel
+//[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+
+// no parallel
+//[assembly: DoNotParallelize]
