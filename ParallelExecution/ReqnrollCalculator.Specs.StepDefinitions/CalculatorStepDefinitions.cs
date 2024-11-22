6a3df772b72acce6f46d4b7b7c2d2e50907fdf90
Gáspár Nagy <gaspar.nagy@gmail.com>
2024-11-22 16:37:27 +0100

    Split LoggingHooks

--- ParallelExecution/ReqnrollCalculator.Specs.StepDefinitions/CalculatorStepDefinitions.cs
+++ ParallelExecution/ReqnrollCalculator.Specs.StepDefinitions/CalculatorStepDefinitions.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Diagnostics;
-using System.Reflection;
 using Reqnroll;
 
 namespace ReqnrollCalculator.Specs.StepDefinitions;
@@ -8,71 +5,13 @@ namespace ReqnrollCalculator.Specs.StepDefinitions;
 [Binding]
 public sealed class CalculatorStepDefinitions
 {
-    private static readonly Stopwatch Stopwatch = new();
-    private static readonly object LockObj = new();
     private static readonly Random Rnd = new(42);
-    private static readonly string LogFileName = $"parallel_log_{typeof(CalculatorStepDefinitions).Assembly.GetName().Name}.txt";
-
-    private static void AppendLogMessage(string message, ITestRunner? testRunner = null, bool newLineAfter = false)
-    {
-        var timestamp = Stopwatch.Elapsed;
-        lock (LockObj)
-        {
-            var line = $"{timestamp:c} {testRunner?.TestWorkerId ?? "-"}: {message}";
-            File.AppendAllLines(LogFileName, newLineAfter ? [line, ""] : [line]);
-        }
-    }
 
     private const bool AddRandomWait = true;
-    private const int MinWaitMs = 100;
-    private const int MaxWaitMs = 150;
+    private const int MinWaitMs = 300;
+    private const int MaxWaitMs = 350;
 
     private readonly Calculator _calculator = new();
-
-    [BeforeTestRun]
-    public static void RunStart()
-    {
-        Stopwatch.Start();
-        AppendLogMessage("BeforeTestRun");
-    }
-
-    [AfterTestRun]
-    public static void RunEnd()
-    {
-        AppendLogMessage("AfterTestRun", newLineAfter: true);
-    }
-
-    [BeforeFeature]
-    public static void FeatureStart(FeatureContext featureContext, ITestRunner testRunner)
-    {
-        AppendLogMessage($"  BeforeFeature: {featureContext.FeatureInfo.Title}", testRunner);
-    }
-
-    [AfterFeature]
-    public static void FeatureEnd(FeatureContext featureContext, ITestRunner testRunner)
-    {
-        AppendLogMessage($"  AfterFeature:  {featureContext.FeatureInfo.Title}", testRunner);
-    }
-
-    private string GetScenarioTitle(ScenarioContext scenarioContext)
-    {
-        if (scenarioContext.ScenarioInfo.Arguments == null || scenarioContext.ScenarioInfo.Arguments.Count == 0)
-            return scenarioContext.ScenarioInfo.Title;
-
-        return $"{scenarioContext.ScenarioInfo.Title} / {string.Join(",", scenarioContext.ScenarioInfo.Arguments.OfType<DictionaryEntry>().Select(a => a.Value))}";
-    }
-
-    [BeforeScenario]
-    public void ScenarioStart(FeatureContext featureContext, ScenarioContext scenarioContext, ITestRunner testRunner)
-    {
-        AppendLogMessage($"    BeforeScenario: {featureContext.FeatureInfo.Title} / {GetScenarioTitle(scenarioContext)}", testRunner);
-    }
-
-    [AfterScenario]
-    public void ScenarioEnd(FeatureContext featureContext, ScenarioContext scenarioContext, ITestRunner testRunner)
-    {
-        AppendLogMessage($"    AfterScenario:  {featureContext.FeatureInfo.Title} / {GetScenarioTitle(scenarioContext)}", testRunner);
-    }
 
     [Given("the first number is {int}")]
     public void GivenTheFirstNumberIs(int number)
---
+++ ParallelExecution/ReqnrollCalculator.Specs.StepDefinitions/LoggingHooks.cs
@@ -0,0 +1,68 @@
+﻿using Reqnroll;
+using System.Collections;
+using System.Diagnostics;
+
+namespace ReqnrollCalculator.Specs.StepDefinitions;
+
+[Binding]
+public class LoggingHooks
+{
+    private static readonly Stopwatch Stopwatch = new();
+    private static readonly object LockObj = new();
+    private static readonly string LogFileName = $"parallel_log_{typeof(CalculatorStepDefinitions).Assembly.GetName().Name}.txt";
+
+    private static void AppendLogMessage(string message, ITestRunner? testRunner = null, bool newLineAfter = false)
+    {
+        var timestamp = Stopwatch.Elapsed;
+        lock (LockObj)
+        {
+            var line = $"{timestamp:c} {testRunner?.TestWorkerId ?? "-"}: {message}";
+            File.AppendAllLines(LogFileName, newLineAfter ? [line, ""] : [line]);
+        }
+    }
+
+    [BeforeTestRun]
+    public static void RunStart()
+    {
+        Stopwatch.Start();
+        AppendLogMessage("BeforeTestRun");
+    }
+
+    [AfterTestRun]
+    public static void RunEnd()
+    {
+        AppendLogMessage("AfterTestRun", newLineAfter: true);
+    }
+
+    [BeforeFeature]
+    public static void FeatureStart(FeatureContext featureContext, ITestRunner testRunner)
+    {
+        AppendLogMessage($"  BeforeFeature: {featureContext.FeatureInfo.Title}", testRunner);
+    }
+
+    [AfterFeature]
+    public static void FeatureEnd(FeatureContext featureContext, ITestRunner testRunner)
+    {
+        AppendLogMessage($"  AfterFeature:  {featureContext.FeatureInfo.Title}", testRunner);
+    }
+
+    private string GetScenarioTitle(ScenarioContext scenarioContext)
+    {
+        if (scenarioContext.ScenarioInfo.Arguments == null || scenarioContext.ScenarioInfo.Arguments.Count == 0)
+            return scenarioContext.ScenarioInfo.Title;
+
+        return $"{scenarioContext.ScenarioInfo.Title} / {string.Join(",", scenarioContext.ScenarioInfo.Arguments.OfType<DictionaryEntry>().Select(a => a.Value))}";
+    }
+
+    [BeforeScenario]
+    public void ScenarioStart(FeatureContext featureContext, ScenarioContext scenarioContext, ITestRunner testRunner)
+    {
+        AppendLogMessage($"    BeforeScenario: {featureContext.FeatureInfo.Title} / {GetScenarioTitle(scenarioContext)}", testRunner);
+    }
+
+    [AfterScenario]
+    public void ScenarioEnd(FeatureContext featureContext, ScenarioContext scenarioContext, ITestRunner testRunner)
+    {
+        AppendLogMessage($"    AfterScenario:  {featureContext.FeatureInfo.Title} / {GetScenarioTitle(scenarioContext)}", testRunner);
+    }
+}
