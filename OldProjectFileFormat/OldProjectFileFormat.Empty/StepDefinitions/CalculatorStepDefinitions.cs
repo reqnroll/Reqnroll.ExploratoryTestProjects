a6d144e02ecda18decf3ee17e100e7f6476648fc
Gáspár Nagy <gaspar.nagy@gmail.com>
2025-06-11 14:29:43 +0200

    Add OldProjectFileFormat.Empty sample

---
+++ OldProjectFileFormat/OldProjectFileFormat.Empty/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,30 @@
+/*using System;
+using Reqnroll;
+
+namespace OldProjectFileFormat.Empty.StepDefinitions
+{
+    [Binding]
+    public class CalculatorStepDefinitions
+    {
+        [Given("the first number is {int}")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+        }
+
+        [Given("the second number is {int}")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+        }
+
+        [Then("the result should be {int}")]
+        public void ThenTheResultShouldBe(int result)
+        {
+        }
+    }
+}
+*/
