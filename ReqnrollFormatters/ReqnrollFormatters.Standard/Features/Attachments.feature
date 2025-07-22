f7c618fd11daba9e56ec642781357279b3cbe174
Gáspár Nagy <gaspar.nagy@gmail.com>
2025-07-22 09:17:14 +0200

    Add ReqnrollFormatters.Standard example

---
+++ ReqnrollFormatters/ReqnrollFormatters.Standard/Features/Attachments.feature
@@ -0,0 +1,8 @@
+﻿@attachment
+Feature: Attachments
+
+A short summary of the feature
+
+Scenario: A scenario with an attachment
+	When the step generates an attachment
+	And the step generates a test output
---
+++ ReqnrollFormatters/ReqnrollFormatters.Standard/StepDefinitions/ProjectStepDefinitions.cs
@@ -0,0 +1,57 @@
+using Reqnroll;
+
+namespace ReqnrollFormatters.Standard.StepDefinitions;
+
+[Binding]
+public sealed class ProjectStepDefinitions(IReqnrollOutputHelper outputHelper)
+{
+    [When("I do something")]
+    public void WhenIDoSomething()
+    {
+    }
+
+    [Then("the scenario fails")]
+    public void ThenTheScenarioFails()
+    {
+        throw new Exception("simulated error");
+    }
+
+    [Given("the first parameter is {string}")]
+    public void GivenTheFirstParameterIs(string firstParam)
+    {
+    }
+
+    [Given("{int} is the second parameter")]
+    public void WhenIsTheSecondParameter(int secondParam)
+    {
+    }
+
+    [Then("the scenario passes")]
+    public void ThenTheScenarioPasses()
+    {
+    }
+
+    [When("the step generates an attachment")]
+    public void WhenTheStepGeneratesAnAttachment()
+    {
+        outputHelper.AddAttachment("sample-image.png");
+    }
+
+    [When("the step generates a test output")]
+    public void WhenTheStepGeneratesATestOutput()
+    {
+        outputHelper.WriteLine("this is a text" + Environment.NewLine + "this is a second line");
+    }
+
+    [BeforeScenario("@attachment")]
+    public void BeforeAttachmentScenario()
+    {
+        outputHelper.WriteLine("before attachment scenario hook executed");
+    }
+
+    [AfterScenario("@attachment")]
+    public void AfterAttachmentScenario()
+    {
+        outputHelper.WriteLine("after attachment scenario hook executed");
+    }
+}
