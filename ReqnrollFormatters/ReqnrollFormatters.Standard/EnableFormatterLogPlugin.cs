68b73e8face7403cfbcc35ba2f9bd27c0ce73fe9
Gáspár Nagy <gaspar.nagy@gmail.com>
2025-09-05 11:52:25 +0200

    ReqnrollFormatters.Standard: add formatter log sample

---
+++ ReqnrollFormatters/ReqnrollFormatters.Standard/EnableFormatterLogPlugin.cs
@@ -0,0 +1,20 @@
+﻿using Reqnroll.Plugins;
+using Reqnroll.UnitTestProvider;
+
+// Remove the comment from the line below to enable the formatter logger
+// [assembly: RuntimePlugin(typeof(Reqnroll.Formatters.RuntimeSupport.EnableFormatterLogPlugin))]
+
+#pragma warning disable IDE0130
+namespace Reqnroll.Formatters.RuntimeSupport;
+#pragma warning restore IDE0130
+
+public class EnableFormatterLogPlugin : IRuntimePlugin
+{
+    public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
+    {
+        runtimePluginEvents.CustomizeGlobalDependencies += (_, args) =>
+        {
+            args.ObjectContainer.RegisterTypeAs<TraceListenerFormatterLog, IFormatterLog>();
+        };
+    }
+}
