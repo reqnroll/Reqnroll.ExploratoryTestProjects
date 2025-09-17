7f97f28b4bd4b754e83f1305cf411518374e2a69
Gáspár Nagy <gaspar.nagy@gmail.com>
2025-09-17 14:47:59 +0200

    Add CleanReqnrollProject/CleanReqnrollProject.Net8.Xunit3Fw

---
+++ CleanReqnrollProject/CleanReqnrollProject.Net8.Xunit3Fw/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,34 @@
+namespace CleanReqnrollProject.StepDefinitions;
+
+[Binding]
+public sealed class CalculatorStepDefinitions
+{
+    private readonly ITestOutputHelper testOutputHelper;
+
+    public CalculatorStepDefinitions(ITestOutputHelper testOutputHelper)
+    {
+        this.testOutputHelper = testOutputHelper;
+    }
+
+    [Given("the first number is {int}")]
+    public void GivenTheFirstNumberIs(int number)
+    {
+    }
+
+    [Given("the second number is {int}")]
+    public void GivenTheSecondNumberIs(int number)
+    {
+    }
+
+    [When("the two numbers are added")]
+    public void WhenTheTwoNumbersAreAdded()
+    {
+        testOutputHelper.WriteLine("numbers added");
+    }
+
+    [Then("the result should be {int}")]
+    public void ThenTheResultShouldBe(int result)
+    {
+        Assert.Equal(result, result);
+    }
+}
