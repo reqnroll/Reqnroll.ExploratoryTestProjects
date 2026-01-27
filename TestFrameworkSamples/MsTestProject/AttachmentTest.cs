3800602c81907c8b8bd17bc840e8413e440c65f3
Gáspár Nagy <gaspar.nagy@gmail.com>
2026-01-27 10:31:35 +0100

    Add attachment tests for TestFrameworkSamples

---
+++ TestFrameworkSamples/MsTestProject/AttachmentTest.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MsTestProject;
+
+[TestClass]
+public class AttachmentTest(TestContext testContext)
+{
+    public TestContext TestContext { get; set; } = testContext;
+
+    [TestMethod]
+    public void TestWithTxtAttachment()
+    {
+        TestContext.AddResultFile(Path.Combine("..", "..", "..", "..", "sample-txt-attachment.txt"));
+    }
+
+    [TestMethod]
+    public void TestWithSameAttachment()
+    {
+        // uses the same attachment as TestWithTxtAttachment
+        TestContext.AddResultFile(Path.Combine("..", "..", "..", "..", "sample-txt-attachment.txt"));
+    }
+
+    [TestMethod]
+    public void TestWithAttachmentOfSameName()
+    {
+        // uses the same attachment as TestWithTxtAttachment
+        string sourceFileName = Path.Combine("..", "..", "..", "..", "sample-other-txt-attachment.txt");
+        File.Copy(sourceFileName, "sample-txt-attachment.txt", true);
+        TestContext.AddResultFile("sample-txt-attachment.txt");
+    }
+
+    [TestMethod]
+    public void TestWithPngAttachment()
+    {
+        TestContext.AddResultFile(Path.Combine("..", "..", "..", "..", "sample-png-attachment.png"));
+    }
+
+}
---
+++ TestFrameworkSamples/NUnitProject/AttachmentTest.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace NUnitProject;
+
+public class AttachmentTest
+{
+
+    [Test]
+    public void TestWithTxtAttachment()
+    {
+        TestContext.AddTestAttachment(Path.Combine("..", "..", "..", "..", "sample-txt-attachment.txt"));
+    }
+
+    [Test]
+    public void TestWithSameAttachment()
+    {
+        // uses the same attachment as TestWithTxtAttachment
+        TestContext.AddTestAttachment(Path.Combine("..", "..", "..", "..", "sample-txt-attachment.txt"));
+    }
+
+    [Test]
+    public void TestWithAttachmentOfSameName()
+    {
+        // uses the same attachment as TestWithTxtAttachment
+        string sourceFileName = Path.Combine("..", "..", "..", "..", "sample-other-txt-attachment.txt");
+        File.Copy(sourceFileName, "sample-txt-attachment.txt", true);
+        TestContext.AddTestAttachment("sample-txt-attachment.txt");
+    }
+
+    [Test]
+    public void TestWithPngAttachment()
+    {
+        TestContext.AddTestAttachment(Path.Combine("..", "..", "..", "..", "sample-png-attachment.png"));
+    }
+
+}
